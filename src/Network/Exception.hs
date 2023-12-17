4fa49d79b373f8667753d34f8a5f484f9bb1e3eb
Deni Lark <denlarsites@gmail.com>
2023-12-17 16:00:58 +0500

    exception handling: getting a web page

--- src/Network/Exception.hs
+++ src/Network/Exception.hs
@@ -1,9 +1,50 @@
 module Network.Exception where
 
-import           Network.HTTP.Conduit           ( HttpException(..) )
+import           Network.HTTP.Conduit              -- ( HttpException(..) )
+import           Network.HTTP.Types             ( Status(statusCode) )
+
 
 handlerHttpException :: HttpException -> IO ()
-handlerHttpException (InvalidUrlException _ _) =
-  putStrLn "Something wrong: InvalidUrlException"
-handlerHttpException (HttpExceptionRequest _ _) =
-  putStrLn "Something wrong: HttpExceptionRequest" -- <> show exceptionContent
+handlerHttpException (InvalidUrlException url _) =
+  putStrLn $ "Invalid url: " <> url
+handlerHttpException (HttpExceptionRequest request exceptionContent) = do
+  let msg = exceptionContentToString exceptionContent
+      url = host request <> path request
+  putStrLn $ show url <> ": " <> msg
+
+responseToStatusCode :: Response () -> String
+responseToStatusCode = show . statusCode . responseStatus
+
+exceptionContentToString :: HttpExceptionContent -> String
+exceptionContentToString c = case c of
+  StatusCodeException response _ ->
+    "StatusCodeException: " <> responseToStatusCode response
+  -- https://wallpaperscraft.ru/asdasda
+
+  TooManyRedirects _                  -> "TooManyRedirects"
+  OverlongHeaders                     -> "OverlongHeaders"
+  ResponseTimeout                     -> "ResponseTimeout"
+  ConnectionTimeout                   -> "ConnectionTimeout"
+
+  ConnectionFailure    _              -> "ConnectionFailure"
+  -- https://wwallpaperscraft.ru/ instead of https://wallpaperscraft.ru/
+
+  InvalidStatusLine    _              -> "InvalidStatusLine"
+  InvalidHeader        _              -> "InvalidHeader"
+  InvalidRequestHeader _              -> "InvalidRequestHeader"
+  InternalException    _              -> "InternalException"
+  ProxyConnectException{}             -> "ProxyConnectException"
+  NoResponseDataReceived              -> "NoResponseDataReceived"
+  TlsNotSupported                     -> "TlsNotSupported"
+  WrongRequestBodyStreamSize _ _      -> "WrongRequestBodyStreamSize"
+  ResponseBodyTooShort       _ _      -> "ResponseBodyTooShort"
+  InvalidChunkHeaders                 -> "InvalidChunkHeaders"
+  IncompleteHeaders                   -> "IncompleteHeaders"
+  InvalidDestinationHost _            -> "InvalidDestinationHost"
+  HttpZlibException      _            -> "HttpZlibException"
+  InvalidProxyEnvironmentVariable _ _ -> "InvalidProxyEnvironmentVariable"
+  ConnectionClosed                    -> "ConnectionClosed"
+  InvalidProxySettings _              -> "InvalidProxySettings"
+
+
+
