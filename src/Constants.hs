b3e051784e321b1376355edcd56a6a99e51c98ec
Deni Lark <denlarsites@gmail.com>
2023-12-19 20:44:14 +0500

    exampes url moved to ExamplesUrl.txt

--- src/Constants.hs
+++ src/Constants.hs
@@ -5,32 +5,3 @@ msgNoImageFound = "No image found"
 
 dirResult :: FilePath
 dirResult = "result"
-
-urlPage1 :: String
-urlPage1 = "https://wallpaperscraft.ru/"
-
-urlPage2 :: String
-urlPage2 = "https://wallpapershq.ru/categories/all/3840x2160"
-
-urlPage3 :: String
-urlPage3 = "https://7themes.su/photo/hd_wallpapers/22"
-
-urlPageWrong1 :: String
-urlPageWrong1 = "https://wwallpaperscraft.ru/"
-
-urlImage1 :: String
-urlImage1 =
-  "https://w0.peakpx.com/wallpaper/586/961/HD-wallpaper-haskell-black-logo-programming-language-grid-metal-background-haskell-artwork-creative-programming-language-signs-haskell-logo.jpg"
-
-urlImage2 :: String
-urlImage2 =
-  "https://raw.githubusercontent.com/pittsburgh-haskell/pittsburgh-haskell-logo/master/pittsburgh-haskell-256.png"
-
-urlImageWrong1 :: String
-urlImageWrong1 =
-  "https://w0.peakpxx.com/wallpaper/586/961/HD-wallpaper-haskell-black-logo-programming-language-grid-metal-background-haskell-artwork-creative-programming-language-signs-haskell-logo.jpg"
-
-urlImageWrong2 :: String
-urlImageWrong2 =
-  "https://raw.ggithubusercontent.com/pittsburgh-haskell/pittsburgh-haskell-logo/master/pittsburgh-haskell-256.png"
-
